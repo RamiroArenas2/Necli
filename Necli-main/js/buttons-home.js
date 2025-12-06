67e6e5e3d63de4a3fcd4f088b6c3a5c5d3d999b9
JsUsuga <juan.usuga@globant.com>
2025-12-05 19:28:12 -0500

    Cambios en el Id de QR

---
+++ Necli-main/js/buttons-home.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded" , () => {
+    const btnSettings = document.querySelector("#button-settings");
+    // const btnIngresa = document.querySelector("#loguin");
+
+    btnSettings.addEventListener("click" , () => {
+        window.location.href = "/Necli-main/pages/settings.html"
+    })
+
+//  btnIngresa.addEventListener("click" , () => {
+//         window.location.href = "/Necli-main/pages/login.html"
+//     })
+});
