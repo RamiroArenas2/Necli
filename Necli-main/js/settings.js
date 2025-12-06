158f484ff1433e0871330d19249358bd741de5aa
RamiroArenas2 <luis.arenas01@globant.com>
2025-12-05 19:08:31 -0500

    actualización settings

--- Necli-main/js/settings.js
+++ Necli-main/js/settings.js
@@ -1,22 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => { 
+document.addEventListener("DOMContentLoaded", () => {
+  const btnBack = document.getElementById("btn-back");
+  const btnUpdateInfo = document.getElementById("btn-update-info");
+  const btnChangePass = document.getElementById("btn-change-pass");
+  const btnDeleteAccount = document.getElementById("btn-delete-account");
+  const btnHelp = document.getElementById("btn-help");
+  const btnLogout = document.getElementById("btn-logout");
 
-    const btnBack = document.getElementById('btn-back');
-    const btnUpdateInfo = document.getElementById('btn-update-info');
-    const btnChangePass = document.getElementById('btn-change-pass');
-    const btnDeleteAccount = document.getElementById('btn-delete-account');
-    const btnHelp = document.getElementById('btn-help');
-    const btnLogout = document.getElementById('btn-logout');
+  btnBack.addEventListener("click", () => {
+    window.location.href = "/Necli.main/pages/home.html";
+  });
 
+  btnLogout.addEventListener("click", () => {
+    const confirmacion = confirm("¿Estás seguro de que deseas cerrar sesión?");
 
-    btnBack.addEventListener('click', () => {
-        window.location.href = "../pages/home.html"
+    if (confirmacion) {
+      console.log("Sesión cerrada");
+      window.location.href = "../index.html";
+    } else {
+      console.log("Cierre de sesión cancelado");
+    }
+
+    const btnUpdateInfo = document.getElementById("btn-update-info");
+    const infoSection = document.getElementById("info-section");
+    const mainContent = document.querySelector(".profile-content");
+    const backInfo = document.getElementById("btn-back-info");
+
+    btnUpdateInfo.addEventListener("click", () => {
+      mainContent.style.display = "none"; // Oculta pantalla principal
+      infoSection.classList.remove("hidden"); // Muestra la cascada
     });
 
-    btnLogout.addEventListener('click', function() {
-        const confirmacion = confirm("¿Estás seguro de que deseas cerrar sesión?");
-        if (confirmacion)
-            console.log("Sesión cerrada");
-        window.location.href = "../index.html"
+    backInfo.addEventListener("click", () => {
+      infoSection.classList.add("hidden"); // Cierra cascada
+      mainContent.style.display = "block"; // Muestra pantalla principal
     });
 
+    btnActInfo.addEventListener("click", () => {});
+  });
 });
